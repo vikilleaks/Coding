ea8ee0f65ed59adcc28693323ed1873065faff26
Vivek Chandela <32481163+vikilleaks@users.noreply.github.com>
2020-03-27 12:31:22 +0530

    My coding template

--- template.cpp
+++ template.cpp
@@ -1,7 +1,4 @@
 #include <bits/stdc++.h>
-#include <ext/pb_ds/assoc_container.hpp> 
-#include <ext/pb_ds/tree_policy.hpp> 
-#define ordered_set tree<int, null_type,less<int>, rb_tree_tag,tree_order_statistics_node_update>
 #define pb push_back
 #define mp make_pair
 #define ff first
@@ -9,7 +6,6 @@
 #define ll long long
 #define MAXN 200005
 using namespace std;
-using namespace __gnu_pbds;  
 
 void solve()
 {
@@ -19,7 +15,7 @@ void solve()
 int main()
 {
 	ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
+    	cin.tie(NULL);
 	cout.tie(NULL);
 	solve();
 }
