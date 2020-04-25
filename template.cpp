e5f7f8bbf3fbde859310af287a8bfab2a26fc41f
Vivek Chandela <32481163+vikilleaks@users.noreply.github.com>
2020-04-25 19:37:56 +0530

    Update template.cpp

--- template.cpp
+++ template.cpp
@@ -4,20 +4,22 @@
 #define ff first
 #define ss second
 #define ll long long
+#define ll_MAX LONG_LONG_MAX
+#define ll_MIN LONG_LONG_MIN
 #define pi pair<int,int>
 #define endl "\n"
-#define MAXN 200005
+#define MAXN 100005
 using namespace std;
 
 void solve()
 {
-	
+    
 }
 
 int main()
 {
-	ios_base::sync_with_stdio(false);
-    	cin.tie(NULL);
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
 	cout.tie(NULL);
-	solve();
+    solve();
 }
