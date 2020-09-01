999906e8afa9500b42f450e83cff9b868945719e
Vivek Chandela <32481163+vikilleaks@users.noreply.github.com>
2020-09-01 12:13:20 +0530

    Dijkstra - SSSP Algo

--- dijkstra.cpp
+++ dijkstra.cpp
@@ -1,83 +1,69 @@
 #include <bits/stdc++.h>
-#include <ext/pb_ds/assoc_container.hpp> 
-#include <ext/pb_ds/tree_policy.hpp> 
-#define ordered_set tree<ll, null_type,less<ll>, rb_tree_tag,tree_order_statistics_node_update>
 #define pb push_back
 #define mp make_pair
 #define ff first
 #define ss second
 #define ll long long
+#define ll_MAX LONG_LONG_MAX
+#define ll_MIN LONG_LONG_MIN
+#define pi pair<ll, ll>
+#define endl "\n"
 #define MAXN 100005
 using namespace std;
-using namespace __gnu_pbds;  
 
-ll n,m,u,v,w,vis[MAXN],d[MAXN],p[MAXN];
-vector<pair<ll,ll> > adj[MAXN];
-multiset<pair<ll,ll> > s;
+ll n, m, a, b, c, vis[MAXN], d[MAXN];
+vector<pi> adj[MAXN];
 
+// T=O(ElogV)
 // algo invariant if graph contains multiple edges or loops
-void dijkstra()
+void dijkstra(ll s)
 {
-	// pairs in a set are sorted by the first value of the pair.
-	s.insert(mp(0,1));
-	while(!s.empty())
-	{
-		pair<ll,ll> t=*s.begin();
-		s.erase(s.begin());
-		ll u=t.ss;
-		if(vis[u]) continue;
-		vis[u]=1;
-		for(ll i=0;i<adj[u].size();i++)
-		{
-			ll v=adj[u][i].ff, w=adj[u][i].ss;
-			if(d[u]+w < d[v])
-			{
-				d[v]=d[u]+w;
-				p[v]=u;
-				s.insert(mp(d[v],v));
-			}
-		}
-	}
+    // pairs in a set are sorted by the first value of the pair.The Dijkstra's algorithm runs for n iterations. 
+    // At each iteration a vertex v is chosen as unmarked vertex which has the least value d[v]:
+    multiset<pi> mm;
+    mm.insert(mp(0, s));
+    while (!mm.empty())
+    {
+        pi p = *mm.begin();
+        mm.erase(mm.begin());
+        ll u = p.ss;
+	// After any vertex v becomes marked, the current distance to it d[v] is the shortest, and will no longer change.
+        if (vis[u] == 1)
+            continue;
+        vis[u] = 1;
+        for (pi t : adj[u])
+        {
+            ll v = t.ff, w = t.ss;
+            if (d[v] > d[u] + w)
+            {
+                d[v] = d[u] + w;
+                mm.insert(mp(d[v], v));
+            }
+        }
+    }
 }
 
 void solve()
 {
-	cin>>n>>m;
-	for(ll i=0;i<m;i++)
-	{
-		cin>>u>>v>>w;
-		adj[u].pb(mp(v,w));
-		adj[v].pb(mp(u,w)); //comment if directed graph
-	}
-	// vertex 1 is source so d[1]=0
-	d[1]=0;
-	for(ll i=2;i<=n;i++) d[i]=LONG_LONG_MAX;
-	dijkstra();
-	if(d[n]==LONG_LONG_MAX) cout<<-1<<endl;
-	else
-	{
-		stack<ll> res;
-		res.push(n);
-		ll q=n;
-		while(q!=1)
-		{
-			res.push(p[q]);
-			q=p[q];
-		}
-		while(!res.empty())
-		{
-			q=res.top();
-			res.pop();
-			cout<<q<<" ";
-		}
-		cout<<endl;
-	}
+    cin >> n >> m;
+    while (m--)
+    {
+        cin >> a >> b >> c;
+        adj[a].pb(mp(b, c));
+    }
+    for (ll i = 1; i <= n; i++)
+        d[i] = ll_MAX;
+    d[1] = 0;
+    dijkstra(1);
+    for (ll i = 1; i <= n; i++)
+        cout << d[i] << " ";
+    cout << endl;
 }
 
 int main()
 {
-	ios_base::sync_with_stdio(false);
+    ios_base::sync_with_stdio(false);
     cin.tie(NULL);
-	cout.tie(NULL);
-	solve();
+    cout.tie(NULL);
+    solve();
 }
