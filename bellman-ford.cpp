8303eb23f9cba619d4a26138d15cbb1b675e51cd
Vivek Chandela <32481163+vikilleaks@users.noreply.github.com>
2020-09-01 13:30:02 +0530

    Bellman-ford algo

--- bellman-ford.cpp
+++ bellman-ford.cpp
@@ -14,15 +14,23 @@ vector<pair<pi,int> > e;
 vector<int> d(MAXN,INT_MAX);
 vector<int> p(MAXN,-1);
 
+// This algorithm can also be applied to graphs containing negative weight edges . 
+// However, if the graph contains a negative cycle, then, clearly, the shortest path to some vertices may not exist. 
+// We can find the negative cycle using Bellman Ford.
+// n−1  phases of the algorithm are sufficient to correctly calculate the lengths of all shortest paths in the graph.
+// Each phase scans through all edges of the graph, and the algorithm tries to produce relaxation along each edge (a,b) having weight c.
+// T=O(n*m)
 void bf(int u)
 {
 	d[u]=0;
 	while(1)
 	{
+		// We will stop after the desired number of phases, not necessarily n-1.
 		int flag=0;
 		for(int i=0;i<m;i++)
 		{
-			// this verification needed if negative weight edges present, else incorrect distance of type INT_MAX-1 etc would appear from vertices to which paths haven't been found. 
+			// This verification is needed if negative weight edges present, else incorrect distance of type INT_MAX-1 etc 
+			// would appear from vertices to which paths haven't been found. 
 			if(d[e[i].ff.ff] < INT_MAX)
 			{
 				if(d[e[i].ff.ss]>d[e[i].ff.ff]+e[i].ss)
